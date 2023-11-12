3f3979bf052e9d750454cf06f295653fe3ef6b0a
CassioSousa09 <148918527+CassioSousa09@users.noreply.github.com>
2023-11-12 17:45:41 +0000

    adicionado funções de salvar, escrever no arquivo e função de leitura de arquivo

--- lab.c
+++ lab.c
@@ -28,3 +28,44 @@ void cadastroT(lista_de_tarefas *tarefas) {
            "tarefas.\n");
   }
 }
+
+//função para salvar tarefas
+void salvarT(lista_de_tarefas *tarefas) {
+  FILE *arquivo = fopen("tarefas_salvas.txt", "w");
+  int numTarefas = 0;
+  if (arquivo) {
+    for (int i = 0; i < numTarefas; i++) {
+      fprintf(arquivo, "%s;%s;%d;%s\n", tarefas[i].descricao,
+              tarefas[i].categoria, tarefas[i].prioridade, tarefas[i].estado);
+    }
+
+    fclose(arquivo);
+
+    printf("Tarefas salvas com sucesso no arquivo 'tarefas_salvas.txt'.\n");
+  } else {
+    printf("Erro ao criar o arquivo 'tarefas_salvas.txt'.\n");
+  }
+}
+// funcao para escrever no arquivo
+void escrever_arq(lista_de_tarefas *tarefas) {
+  int numTarefas = 0;
+  int tamanho = numTarefas;
+  FILE *binario;
+  binario = fopen("file.bin", "wb");
+  if (binario) {
+    fwrite(tarefas, sizeof(lista_de_tarefas), tamanho, binario);
+    fclose(binario);
+  }
+}
+//funcao para leitura de arquivo
+int leitura_arquivo(lista_de_tarefas tarefas[], int maxTarefas) {
+  FILE *binario = fopen("file.bin", "rb");
+  int cont = 0;
+  if (binario) {
+    while (cont < maxTarefas &&
+           fread(tarefas, sizeof(lista_de_tarefas), 1, binario) == 1) {
+      cont = cont + 1;
+      fclose(binario);
+    }
+  }
+}
