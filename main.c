112f87336746c1887c5374e2ac4121754bfe5a98
CassioSousa09 <148918527+CassioSousa09@users.noreply.github.com>
2023-11-12 23:58:05 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
-#include "lab.h"
 #include <stdio.h>
 #include <string.h>
+#include "lab.h"
 
 lista_de_tarefas tarefas[100];
 
+
 int main() {
   int escolha;
-  lista_de_tarefas lista_de_tarefas[100];
-  leitura_arquivo(lista_de_tarefas, 100);
+
   do {
     printf("\nMenu:\n");
     printf("1. Cadastrar Tarefa\n");
@@ -26,51 +26,51 @@ int main() {
     scanf("%d", &escolha);
 
     switch (escolha) {
-        //digite 1 para cadastro
+      // digite 1 para cadastro
     case 1:
       cadastroT(tarefas);
       break;
-      //digite 2 para tarefas
+      // digite 2 para tarefas
     case 2:
       listarT(tarefas);
       break;
-      //digite 3 para deletar
+      // digite 3 para deletar
     case 3:
       deletarT(tarefas);
       break;
-//digite 4 para realizar alterações
+      // digite 4 para realizar alterações
     case 4:
       alteracao(tarefas);
       break;
-//digite 5 para filtro de prioridades 
+      // digite 5 para filtro de prioridades
     case 5:
       Filtro_Prioridade(tarefas);
       break;
-    //digite 6 para filtro estado
+    // digite 6 para filtro estado
     case 6:
       Filtro_Estado(tarefas);
       break;
-//digite 7 para filtrar categoria ordenada
+      // digite 7 para filtrar categoria ordenada
     case 7:
       filtrarCategoriaOrdenada(tarefas);
       break;
-//digite 8 para filtro prioridade e categoria
+      // digite 8 para filtro prioridade e categoria
     case 8:
       filtroPrioridadeECategoria(tarefas);
       break;
-//digite 9 exportar por prioridade
+      // digite 9 exportar por prioridade
     case 9:
       exportarPorPrioridade(tarefas);
       break;
-//digite 10 exportar cto
+      // digite 10 exportar cto
     case 10:
       exportarCto(tarefas);
       break;
-//digite 11 para exportar prioridade e categoria
+      // digite 11 para exportar prioridade e categoria
     case 11:
       exportarPrioridadeCategoria(tarefas);
       break;
- //digite 12 para sair"
+      // digite 12 para sair"
     case 12:
       escrever_arq(tarefas);
       printf("Saindo...\n");
